4dff5c4ba1ad77200c6f73476e8bb10bd438df75
Henry Choo <lchoo9294@gmail.com>
2022-02-28 22:24:43 +0800

    refactor: remove test

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -13,7 +13,6 @@ type Props = FooterProps;
 const Footer: React.FC<Props> = () => {
   return (
     <>
-      {/* test */}
       <footer className="footer__outerdiv">
         <Container>
           <div className="footer__top">
